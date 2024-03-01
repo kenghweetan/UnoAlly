418d3c33568c59fcbb1d9a590cdfc52dfe8077bb
Tan Keng Hwee <tankenghwee1996@gmail.com>
2024-03-02 07:11:07 +0800

    Add project files.

---
+++ UnoAlly.Server/GameServer.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace UnoAlly.Server
+{
+    public class GameServer
+    {
+        public string? roomId;
+        public string? Name;
+        public string? roomPassword;
+
+    }
+}
---
+++ UnoAlly.Server/Program.cs
@@ -0,0 +1,14 @@
+using UnoAlly.Server.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSignalR();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapHub<UnoHub>("/unohub");
+
+app.Run();
